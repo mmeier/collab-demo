67da4e9005da981a58867d1b03df879811290473
Marcus <marcus@mdmeier.com>
2025-10-18 23:01:37 -0500

    document models

--- src/dnd_characters/models.py
+++ src/dnd_characters/models.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel, Field
 from uuid import uuid4
 
 
+# The character model is defined as a pydantic model.
+#
 class Character(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid4()))
     character_name: str
